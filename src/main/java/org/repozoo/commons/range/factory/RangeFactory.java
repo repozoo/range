1bd8e6c4d9be75d166a4ef103a9ae3fbe9305bd3
Yves Nzinga Kempter <yves.nzinga-kempter@schuetze.ag>
2022-04-10 10:39:47 +0200

    refactoring: make CreateRangeBuilder inner class of RangeFactory

--- src/main/java/org/repozoo/commons/range/factory/RangeFactory.java
+++ src/main/java/org/repozoo/commons/range/factory/RangeFactory.java
@@ -1,6 +1,11 @@
 package org.repozoo.commons.range.factory;
 
 import org.repozoo.commons.range.Range;
+import org.repozoo.commons.range.ValueIterator;
+import org.repozoo.commons.range.value.Value;
+
+import java.util.Comparator;
+import java.util.function.UnaryOperator;
 
 public class RangeFactory {
 
@@ -12,5 +17,31 @@ public class RangeFactory {
     public static <Y> CreateRangeBuilder<Y> forType(Class<Y> rangeType) {
         //until now, rangeType is syntactic sugar only
         return new CreateRangeBuilder<>();
+    }
+
+    public static class CreateRangeBuilder<Y> {
+
+        private Comparator<Y> comparator;
+        private ValueIterator<Y> iterator;
+
+        CreateRangeBuilder() {}
+
+        public CreateRangeBuilder<Y> withIterator(UnaryOperator<Y> next, UnaryOperator<Y> previous) {
+            this.iterator = new ValueIterator<>(next, previous);
+            return this;
+        }
+
+        public CreateRangeBuilder<Y> withComparator(Comparator<Y> comparator) {
+            this.comparator = comparator;
+            return this;
+        }
+
+        public CreateRange<Y> build() {
+            return (from, to) -> {
+                Value<Y> start = Value.of(from, iterator, comparator);
+                Value<Y> end = Value.of(to, iterator, comparator);
+                return Range.between(start, end);
+            };
+        }
     }
 }
